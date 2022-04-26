e43556d10b66575af81f5eec6e9bcfa0b456ed51
JonathanIsCodeing <jonathangruss@hotmail.com>
2022-04-26 21:31:55 +0200

    first solution

--- ant-colony.py
+++ ant-colony.py
@@ -1,4 +1,8 @@
+import operator
+import random
+
 import numpy as np
+import matplotlib.pyplot as plt
 
 from environment import Environment
 from ant import Ant 
@@ -29,7 +33,7 @@ class AntColony:
         for i in range(ant_population):
             
             # Initialize an ant on a random initial location 
-            ant = Ant(self.alpha, self.beta, None)
+            ant = Ant(self.alpha, self.beta, random.choice(self.environment.get_possible_locations()))
 
             # Position the ant in the environment of the ant colony so that it can move around
             ant.join(self.environment)
@@ -39,25 +43,76 @@ class AntColony:
 
     # Solve the ant colony optimization problem  
     def solve(self):
+        self.environment.initialize_pheromone_map(self.ant_population)
 
-        # The solution will be a list of the visited cities
-        solution = []
+        differences = []
+        for _ in range(self.iterations):
+            distances = []
+            best_ant = 0
+            best_distance = np.inf
+            for i, ant in enumerate(self.ants):
+                ant.run()
+                distances.append(ant.travelled_distance)
+                if ant.travelled_distance < best_distance:
+                    best_ant = i
+                    best_distance = ant.travelled_distance
 
-        # Initially, the shortest distance is set to infinite
-        shortest_distance = np.inf
+            differences.append(max(distances) - min(distances))
+            plot(self.environment.coordinates, self.ants[best_ant].path, _)
 
+            self.environment.update_pheromone_map(self.ants)
+            print(f'Pheromone: Min {min(self.environment.get_pheromone_map())}\n Max {max(self.environment.get_pheromone_map())}')
+
+        # plotting
+        x = np.arange(1, self.iterations + 1)
+        y = np.array(differences)
+        plt.plot(x, y, color="green")
+        plt.show()
+
+        shortest_distance, solution = np.inf, []
+        for ant in self.ants:
+            if ant.travelled_distance < shortest_distance:
+                shortest_distance = ant.travelled_distance
+                solution = ant.path
         return solution, shortest_distance
 
 
+def plot(points, path: list, idx=0):
+    x = []
+    y = []
+    for point in points:
+        x.append(point[0])
+        y.append(point[1])
+    # noinspection PyUnusedLocal
+    y = list(map(operator.sub, [max(y) for i in range(len(points))], y))
+    plt.plot(x, y, 'co')
+
+    for _ in range(0, len(path)):
+        i = path[_ - 1] - 1
+        j = path[_] - 1
+        # noinspection PyUnresolvedReferences
+        plt.arrow(x[i], y[i], x[j] - x[i], y[j] - y[i], color='r', length_includes_head=True)
+
+    # noinspection PyTypeChecker
+    plt.xlim(0, max(x) * 1.1)
+    # noinspection PyTypeChecker
+    plt.ylim(0, max(y) * 1.1)
+    #plt.show()
+    plt.savefig(f'plots/path_{idx:04d}.png')
+    plt.cla()
+
+
 def main():
-    # Intialize the ant colony
-    ant_colony = AntColony(1, None, None, None, None)
+    # Initialize the ant colony
+    ant_colony = AntColony(50, 10, 1, 5, 0.1)
 
     # Solve the ant colony optimization problem
     solution, distance = ant_colony.solve()
     print("Solution: ", solution)
     print("Distance: ", distance)
 
+    plot(ant_colony.environment.coordinates, solution)
+
 
 if __name__ == '__main__':
-    main()    
+    main()
--- environment.py
+++ environment.py
@@ -1,4 +1,20 @@
 import math
+import tsplib95
+import numpy as np
+import tspsolve
+
+
+class Graph(object):
+    def __init__(self, cost_matrix: list, dim: int):
+        """
+        :param cost_matrix:
+        :param dim: dim of the cost matrix
+        """
+        self.matrix = cost_matrix
+        self.rank = dim
+        # noinspection PyUnusedLocal
+        self.pheromone = [[1 / (dim * dim) for j in range(dim)] for i in range(dim)]
+
 
 # Class representing the environment of the ant colony
 """
@@ -6,29 +22,46 @@ import math
 """
 class Environment:
     def __init__(self, rho):
+        self.rho = rho
 
-        self.rho =rho
-        
         # Initialize the environment topology
+        self.topology = tsplib95.load('att48-specs/att48.tsp')
+        self.dim = self.topology.dimension
+        self.coordinates = list(self.topology.node_coords.values())
 
-        # Intialize the pheromone map in the environment
+        # Initialize the pheromone map in the environment
+        self.pheromone = []
 
-        pass 
-
-    # Intialize the pheromone trails in the environment
-    def initialize_pheromone_map(self):
-        pass
+    def initialize_pheromone_map(self, ant_count):
+        value = ant_count / self._Cnn()
+        self.pheromone = [[value] * self.dim for _ in range(self.dim)]
+        print(f'Initial pheromone value: {value}')
 
     # Update the pheromone trails in the environment
-    def update_pheromone_map(self):
-        pass
+    def update_pheromone_map(self, ants: list):
+        for i, row in enumerate(self.pheromone):
+            for j, col in enumerate(row):
+                self.pheromone[i][j] *= self.rho
+                for ant in ants:
+                    self.pheromone[i][j] += ant.pheromone_delta[i][j]
 
     # Get the pheromone trails in the environment
     def get_pheromone_map(self):
-        pass
-    
+        return self.pheromone
+
+    def get_distance(self, i, j):
+        return self.topology.get_weight(i, j)
+
     # Get the environment topology
     def get_possible_locations(self):
-        pass
+        return list(self.topology.get_nodes())
 
-    
+    def _Cnn(self):
+        cost_matrix = np.array([[self.get_distance(i, j) for i in range(1, self.dim + 1)] for j in range(1, self.dim + 1)])
+        path = tspsolve.nearest_neighbor(cost_matrix)
+
+        return sum([cost_matrix[path[i - 1], path[i]] for i in range(self.dim)])
+
+
+if __name__ == '__main__':
+    environment = Environment(0.1)
